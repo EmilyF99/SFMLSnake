45337dfc72000c45a1219d176253b25e2f645c97
Emily Fletcher <emilyfletcher099@gmail.com>
2024-05-06 02:54:55 +0100

    Add project files.

---
+++ SFMLByExample/Game.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Window.h"
+#include "Snake.h"
+#include "World.h"
+#include "Textbox.h"
+
+class Game {
+public:
+	Game();
+	~Game();
+	void HandleInput();
+	void Update();
+	void Render();
+	Window* GetWindow();
+	sf::Time GetElapsed();
+	void RestartClock();
+private:
+	Window m_window;
+	sf::Clock m_clock;
+	float m_elapsed;
+	World m_world;
+	Snake m_snake;
+	Textbox m_textbox;
+};
